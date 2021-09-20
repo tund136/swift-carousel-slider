e4d5d9acb58b7d55c87999aa4219d2b1049fb8d5
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-09-20 20:55:05 +0700

    Create the project

---
+++ CarouselSlider/CarouselSliderApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarouselSliderApp.swift
+//  CarouselSlider
+//
+//  Created by Danh Tu on 20/09/2021.
+//
+
+import SwiftUI
+
+@main
+struct CarouselSliderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
